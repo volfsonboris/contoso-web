e414fd87616af771c558834e224e4996b539260f
volfsonboris <167210985+volfsonboris@users.noreply.github.com>
2024-04-19 11:52:34 -0400

    Create next-sitemap.js

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+siteUrl: process.env.SITE_URL || 'https://red-sky-0afc29210.5.azurestaticapps.net',
+generateRobotsTxt: true, // (optional)
+// REST CODE READ DOCS  ...
+}
